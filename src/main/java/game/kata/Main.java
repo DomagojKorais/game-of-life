c4c2e389d6c03c830725114fa164d8e3ba5d969a
Diego Baldassar <diego@baldassar.net>
2019-02-04 19:38:33 +0100

    #6 Diego: Service commit

--- src/main/java/game/kata/Main.java
+++ src/main/java/game/kata/Main.java
@@ -1,8 +1,13 @@
 package game.kata;
 
+import java.io.FileNotFoundException;
+import java.util.Objects;
+
 public class Main {
-    public static void main() {
+    public static void main() throws FileNotFoundException {
 
-
+        String filename = Objects.requireNonNull(Main.class.getClassLoader().getResource("grid.txt")).getFile();
+        GridReader reader = new GridReader();
+        Grid grid = reader.parseGridFromFile(filename);
     }
 }
