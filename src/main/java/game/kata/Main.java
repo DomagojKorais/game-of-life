a161b9a4596349c7f2fbad8efb01a3aff970f1ef
Diego Baldassar <diego@baldassar.net>
2019-02-04 22:15:40 +0100

    #6 Diego: Main fix
    - Temporarily added grid.txt under production. FIX THIS!

--- src/main/java/game/kata/Main.java
+++ src/main/java/game/kata/Main.java
@@ -1,14 +1,14 @@
 package game.kata;
 
-
+import java.io.FileNotFoundException;
 import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //parte non funzionante
         System.out.println("a");
         GridReader reader = new GridReader();
-        String filename = Objects.requireNonNull(GridReader.class.getClassLoader().getResource("grid.txt")).getFile();
+        String filename = Objects.requireNonNull(Main.class.getClassLoader().getResource("grid.txt")).getFile();
         System.out.println(filename);
         Grid grid = reader.parseGridFromFile(filename);
         //fine parte non funzionante
