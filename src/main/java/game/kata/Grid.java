dfaa6f1ed05b15550af92ac3969c2ac726760f89
ginevracoal <ginevracoal@gmail.com>
2019-02-05 09:29:12 +0100

    #7 Grid refactoring

--- src/main/java/game/kata/Grid.java
+++ src/main/java/game/kata/Grid.java
@@ -17,8 +17,7 @@ class Grid {
         cellMatrix = new Cell[rows][columns];
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < columns; ++j) {
-                int aliveNeighbours = countAliveNeighbors(_intMatrix,i,j);
-                cellMatrix[i][j] = new Cell(_intMatrix[i][j], aliveNeighbours);
+                cellMatrix[i][j] = new Cell(_intMatrix[i][j], countAliveNeighbors(_intMatrix,i,j));
             }
         }
     }
@@ -26,15 +25,11 @@ class Grid {
     public int countAliveNeighbors(int[][] _intMatrix, int row, int col){
         int aliveNeighbours = 0;
 
-        // ogni cella viva contribuisce
-
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++)
                 if (row+i>=0 && col+j>=0 && row+i < rows && col+j<columns)
                     aliveNeighbours += _intMatrix[row + i][col + j];
         }
-        // The cell needs to be subtracted from
-        // its neighbours as it was counted before
         aliveNeighbours -= _intMatrix[row][col];
 
         return aliveNeighbours;
@@ -45,10 +40,6 @@ class Grid {
         return cellMatrix[i][j];
     }
 
-    public Cell[][] getCellMatrix(){
-        return cellMatrix;
-    }
-
     public void printGrid(){
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < columns; ++j)
@@ -57,15 +48,13 @@ class Grid {
         }
     }
 
+
     public Grid evolve(){
         int[][] newIntMatrix = new int[rows][columns];
 
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < columns; ++j) {
-                Cell cell = cellMatrix[i][j];
-                boolean sentence = judge(cell);
-                int status = cell.evolve(sentence);
-                newIntMatrix[i][j] = status;
+                newIntMatrix[i][j] = cellMatrix[i][j].evolve(judge(cellMatrix[i][j]));
             }
         }
 
