e2dacf1f77a1f2d0e282ecb81e3697e879140de0
doma <domagoj.korais@gmail.com>
2019-02-13 20:15:02 +0100

    Minor cell refactoring

--- src/main/java/game/kata/Cell.java
+++ src/main/java/game/kata/Cell.java
@@ -3,15 +3,13 @@ package game.kata;
 import static game.kata.Game.judge;
 
 public class Cell {
-    private final int status;
 
+    private final int status;
     public Cell (int inception) {
         this.status = inception;
     }
-
     Cell evolve(int aliveNeighbours) {
         return judge(this, aliveNeighbours);
     }
-
     public int getStatus() { return status; }
 }
